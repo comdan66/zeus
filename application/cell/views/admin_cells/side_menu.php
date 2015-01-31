efc786c5378d7a318d5d1304aef01cfca3d46864
OA Wu <comdan66@gmail.com>
2015-01-31 20:11:23 +0800

    Modify admin main views lang.

--- application/cell/views/admin_cells/side_menu.php
+++ application/cell/views/admin_cells/side_menu.php
@@ -1,11 +1,10 @@
-
 <aside class="grid col-one-quarter mq2-col-one-third mq3-col-full">
   <menu>
     <div id="wrapper">
       <ul class="menu">
-        <li class="item5"><a href="#">Admin</a>
+        <li class="item5"><a href="#">管理員</a>
           <ul>
-            <li class="subitem1"><a href="/admin/edit">Update admin</a></li>
+            <li class="subitem1"><a href="/admin/edit">更新管理員資料</a></li>
           </ul>
         </li>
       </ul>
