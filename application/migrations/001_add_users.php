6e8e9049b690417e488aec9185db5288073eac28
OA Wu <comdan66@gmail.com>
2015-01-31 19:24:03 +0800

    Add user model and migrate.

--- application/migrations/001_add_users.php
+++ application/migrations/001_add_users.php
@@ -9,32 +9,20 @@ class Migration_Add_users extends CI_Migration {
     $this->db->query (
       "CREATE TABLE `users` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
-        `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-        `avatar` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+        `account` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+        `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+        `login_count` int(11) NOT NULL DEFAULT '0',
+        `logined_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "',
         `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "',
         `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "',
-        PRIMARY KEY (`id`)
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
-    );
-
-    $this->db->query (
-      "CREATE TABLE `delete_users` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `origin_id` int(11) NOT NULL,
-        `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-        `avatar` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-        `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "',
-        `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "',
-        PRIMARY KEY (`id`)
+        PRIMARY KEY (`id`),
+        KEY `account_password_index` (`account`, `password`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
     );
   }
   public function down () {
     $this->db->query (
       "DROP TABLE `users`;"
-    );
-    $this->db->query (
-      "DROP TABLE `delete_users`;"
     );
   }
 }
--- application/models/User.php
+++ application/models/User.php
@@ -9,11 +9,9 @@ class User extends OaModel {
   static $table_name = 'users';
 
   static $has_many = array (
-    array ('pictures', 'class_name' => 'Picture')
   );
 
   public function __construct ($attributes = array (), $guard_attributes = true, $instantiating_via_find = false, $new_record = true) {
     parent::__construct ($attributes, $guard_attributes, $instantiating_via_find, $new_record);
-    OrmImageUploader::bind ('avatar');
   }
 }
