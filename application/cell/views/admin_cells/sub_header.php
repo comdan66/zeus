6a68338cb13fca67e58645dcb99061993f9bca1e
OA Wu <comdan66@gmail.com>
2015-02-01 00:50:21 +0800

    Changes admin logo.

--- application/cell/views/admin_cells/sub_header.php
+++ application/cell/views/admin_cells/sub_header.php
@@ -2,9 +2,9 @@
   <hr>
 <?php
   if (identity ()->user ()) { ?>
-    <p class="fleft">綠迷國際有限公司</p>
+    <p class="fleft">宙思設計有限公司</p>
     <strong>
-      <a href="<?php echo base_url (array ('admin', 'login'));?>" class="alignright">lumiere(<?php echo identity ()->user ()->account;?>)</a>
+      <a href="<?php echo base_url (array ('admin', 'login'));?>" class="alignright">zeus(<?php echo identity ()->user ()->account;?>)</a>
     </strong>
 <?php
   } ?>
