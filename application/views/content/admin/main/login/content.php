886cfd788dc36c1d62adbcaa1c4c57f781956cb3
OA Wu <comdan66@gmail.com>
2015-02-01 17:24:55 +0800

    Fix layout.

--- application/views/content/admin/main/login/content.php
+++ application/views/content/admin/main/login/content.php
@@ -1,5 +1,5 @@
 <section class="grid col-three-quarters mq2-col-full">
-  <h2>某某公司 <?php echo identity ()->user ()->account;?> 您好：</h2>
+  <h2>宙思設計有限公司 <?php echo identity ()->user ()->account;?> 您好：</h2>
   <hr>
   <article id="navplace">
     <h3>您上次最後一次登入的時間日期為：<?php echo identity ()->user ()->logined_at->format ('Y年m月d日');?></h3>
