9b36b8f921eec7463b67cca10f6fce2d9bd92cf8
OA Wu <comdan66@gmail.com>
2015-02-24 22:02:54 +0800

    Modify

--- application/views/content/site/contacts/index/content.php
+++ application/views/content/site/contacts/index/content.php
@@ -14,7 +14,7 @@
       宙思設計有限公司<br />
       ZEUS DESIGN
     </h6>
-    
+
     <p class="mbottom">
       235 新北市中和<br >
       區中和路232號5樓之1 <br >
@@ -23,15 +23,9 @@
       Taiwan (R.O.C.)<br >
     </p>
 
-    <br />
-    
     <p class="mbottom">
       TEL 02 8242 3885<br />
       FAX 02 8242 3885
-    </p>
-
-    <p class="mbottom">
-      <a href="mailto:info@zeusdesign.com.tw">info@zeusdesign.com.tw</a>
     </p>
 
     <p class="mbottom">
@@ -40,13 +34,13 @@
       10:00 to 18:00
     </p>
   </aside>
-  
+
   <section class="grid col-three-quarters mq2-col-two-thirds mq3-col-full">
     <h2>Drop us a message</h2>
 <?php if (isset ($message) && $message) { ?>
         <p class="warning"><?php echo $message;?></p>
 <?php } else { ?>
-        <form id="contact_form" class="contact_form" action="<?php echo base_url (array ('contacts', 'submit'));?>" method="post" name="contact_form">  
+        <form id="contact_form" class="contact_form" action="<?php echo base_url (array ('contacts', 'submit'));?>" method="post" name="contact_form">
           <ul>
             <li>
               <label for="name">Your name:</label>
@@ -55,17 +49,17 @@
             <li>
               <label for="email">Your Email:</label>
               <input type="text" name="email" id="email" class="required email" value="" placeholder='EX: info@zeusdesign.com.tw' pattern=".{1,}" required title="Your Email!" />
-            </li> 
+            </li>
             <li>
               <label for="message">Message:</label>
               <textarea name="message" id="message" cols="100" rows="6" class="required" pattern=".{1,}" required title="Your Message!" ></textarea>
             </li>
             <li>
               <button type="submit" id="submit" name="submit" class="button fright">Send it</button>
-            </li> 
-          </ul>     
+            </li>
+          </ul>
         </form>
 <?php }?>
 
-  </section>  
+  </section>
 </div>
